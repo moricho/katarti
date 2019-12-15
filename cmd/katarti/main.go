074357de0e22624a5394c247d00f57ead8d6cd51
moricho <ikeda.morito@gmail.com>
2019-12-15 21:06:24 +0900

    init logger & config in main.go

--- cmd/katarti/main.go
+++ cmd/katarti/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"github.com/moricho/katariti/config"
+	"github.com/moricho/katariti/internal/driver/logger"
+)
 
+func main() {
+	conf, err := config.New()
+	if err != nil {
+		panic(err)
+	}
+
+	logger, err := logger.New()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+
+	return
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/moricho/katarti
 
 go 1.13
+
+require (
+	github.com/pkg/errors v0.8.0
+	github.com/spf13/viper v1.6.1
+	go.uber.org/zap v1.10.0
+)
